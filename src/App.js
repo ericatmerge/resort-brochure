5c21bd998ca5c5969985c69169c6aa0cbc2b1a7e
Ryan Reece <rreece@mergeworld.com>
2024-04-30 15:23:18 -0400

    Start building out page and design functions and presumably add intersection observer in case necessary.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from "./logo.svg"
-import "./App.scss"
+import React from 'react';
+import { motion} from 'framer-motion';
 
 function App() {
+
+  const images = [
+    { id: 1, src: 'https://images.pexels.com/photos/10962931/pexels-photo-10962931.jpeg?auto=compress&cs=tinysrgb&w=800', text: 'Discover the Manatee Fit Zone, where your wellness journey begins amidst serene surroundings. Our gym, adorned with subtle manatee-themed touches, offers state-of-the-art equipment and panoramic nature views for an inspiring workout experience.' },
+    { id: 2, src: 'https://images.pexels.com/photos/457882/pexels-photo-457882.jpeg?auto=compress&cs=tinysrgb&w=800', text: 'Escape to the Manatee Serenity Spa, a haven of relaxation where modern stresses fade away. With gentle manatee-themed touches throughout, our skilled therapists offer personalized treatments to restore your mind, body, and spirit to harmony.' },
+    { id: 3, src: 'https://images.pexels.com/photos/136040/pexels-photo-136040.jpeg?auto=compress&cs=tinysrgb&w=800', text: 'Indulge in coastal delights at Tranquil Tides Eatery, where every dish celebrates fresh flavors and mindful dining. Immerse yourself in our serene restaurant ambiance, subtly accented with manatee-inspired decor, for a culinary journey like no other.' },
+    // Add more images as needed
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="#"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div>
+        <h1 className="text-3xl">Welcome to Serenity</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi turpis erat, aliquet ut felis quis, vehicula eleifend ante. Nulla in placerat velit. Vestibulum sodales tincidunt fermentum.</p>
+      </div>
+      {images.map(image => (
+        <motion.div
+          key={image.id}
+          className="grid gap-5"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: Math.random() * 1 }} // Random delay for stagger effect
         >
-          Future home of our Resort
-        </a>
-      </header>
-    </div>
+          <div className="relative">
+            <img src={image.src} alt={`Image ${image.id}`} className="object-cover" />
+            <p className="absolute bottom-0 left-0 w-full text-white hidden">{image.text}</p>
+          </div>
+        </motion.div>
+      ))}
+    </>
   )
 }
 
